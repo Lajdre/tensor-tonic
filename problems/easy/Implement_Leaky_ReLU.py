0d782b07bfab43070ef24877c0ff9e57c7c07e4d
Lajdre <AndreAndreM@proton.me>
2026-06-01 23:50:18 +0200

    add implement leaky relu

---
+++ problems/easy/Implement_Leaky_ReLU.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def leaky_relu(x, alpha=0.01):
+  """Vectorized Leaky ReLU implementation."""
+  x_arr = np.asarray(x, dtype=float)
+  return np.where(x_arr >= 0, x_arr, alpha * x_arr)
